b8d801a135e7bda5843317a303f441747038948e
5thwin <hisy2059@naver.com>
2025-08-05 17:43:35 +0900

    FEAT: Raid Sample data: visible

---
+++ src/data/sample.ts
@@ -0,0 +1,20 @@
+//레이드 플래너에 필요한 정보
+//메인화면에 띄울 해당 공대가 도는 레이드 정보
+const RAID = {
+	visible: {
+		valtan: false,
+		biakiss: false,
+		'kakul-saydon': false,
+		abrelshud: false,
+		illiakan: false,
+		kayangel: false,
+		kamen: false,
+		echidna: false,
+		behemoth: false,
+		act1: true,
+		act2: true,
+		act3: true,
+		act4: true,
+		denouement: true,
+	},
+};
